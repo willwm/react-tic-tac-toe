59928fc9b81bda04c164692023ac81cfa4033c85
Will Wolff-Myren <willwm@gmail.com>
2025-12-23 05:06:34 +0000

    Refactor to use `useState`

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-function Square({ value }) {
+import { useState } from "react";
+
+function Square({ initialValue }) {
+  const [value, setValue] = useState(initialValue);
+
   function handleClick(val) {
     console.log(`${val} clicked!`);
+    setValue("X");
   }
 
   return (
@@ -14,19 +19,19 @@ export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square initialValue="1" />
+        <Square initialValue="2" />
+        <Square initialValue="3" />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square initialValue="4" />
+        <Square initialValue="5" />
+        <Square initialValue="6" />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square initialValue="7" />
+        <Square initialValue="8" />
+        <Square initialValue="9" />
       </div>
     </>
   );
